95bd0cad52c958d15133448d6a9190df7d5dd16d
Gayatrix10 <contact.gayatri.gawade@gmail.com>
2026-06-06 23:45:25 +0530

    Initial commit

---
+++ loops.java
@@ -0,0 +1,26 @@
+public class loops{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.print(i+" ");
+        }
+        int i=1;
+        while(i<=50){
+            System.out.print(i+" ");
+            i++;
+        }
+        
+int table=0;
+int n=2;
+        for(int ii=1;ii<=10;ii++){
+        System.out.println("");
+            table=n*ii;
+            System.out.println(n+" X "+ii+" = "+table);
+        }
+        
+        
+    }
+}  
+
+    
